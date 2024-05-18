654253655f96faa3ddba6f3f386891633ab87bea
xiaoliziawa <xiaoliziawa@gmail.com>
2024-05-18 23:36:37 +0800

    Wood Crafting Optimize

--- kubejs/server_scripts/Loots/Tool.js
+++ kubejs/server_scripts/Loots/Tool.js
@@ -1,20 +1,22 @@
 LootJS.modifiers(loots => {
 	let HandLoots = [
 		// 圆石=>砂砾
-		['#forge:cobblestone', '#new_create:hammer', 'minecraft:gravel'],
+		['#forge:cobblestone', '#new_create:hammer', 'minecraft:gravel', 0.5],
 		// 石头=>砂砾
-		['minecraft:stone', '#new_create:hammer', 'minecraft:gravel'],
+		['minecraft:stone', '#new_create:hammer', 'minecraft:gravel', 0.5],
 		// 砂砾=>沙子
-		['#forge:gravel', '#new_create:hammer', 'minecraft:sand'],
+		['#forge:gravel', '#new_create:hammer', 'minecraft:sand', 0.5],
 		// 砂石=>沙子
-		['#forge:sandstone', '#new_create:hammer', 'minecraft:sand'],
+		['#forge:sandstone', '#new_create:hammer', 'minecraft:sand', 0.5],
 		// 沙子=>尘土
-		['#forge:sand', '#new_create:hammer', 'new_create:dust']
+		['#forge:sand', '#new_create:hammer', 'new_create:dust', 0.5],
+		// 草=>杂草
+		['minecraft:grass', '#forge:tools/knives', 'new_create:grass_fiber', 1.0]
 	]
-	HandLoots.forEach(([Blocks, Tools, Loots]) => {
+	HandLoots.forEach(([Blocks, Tools, Loots, Chance]) => {
 		loots.addBlockLootModifier(Blocks)
 			.matchMainHand(Tools)
 			.addAlternativesLoot(LootEntry.of(Loots)
-				.when(loots => loots.randomChance(0.5)))
+				.when(loots => loots.randomChance(Chance)))
 	})
 })
--- kubejs/server_scripts/Recipes/Wood.js
+++ kubejs/server_scripts/Recipes/Wood.js
@@ -1,18 +1,20 @@
 ServerEvents.recipes(event => {
-	Ingredient.of('#minecraft:logs').getItemIds().forEach(e => {
-		let pos = e.indexOf(':')
-		let result = e.substring(0, pos + 1) + 'stripped_' + e.substring(pos + 1)
-		event.custom({
-			"type": "farmersdelight:cutting",
-			"ingredients": [{ "item": e }],
-			"result": [
-				{ "item": result },
-				{ "item": "farmersdelight:tree_bark" }
+	const logIDs = Ingredient.of('#minecraft:logs').getItemIds()
+	const Recipes = logIDs.map(logID => {
+		const [namespace, item] = logID.split(':')
+		const strippedLogID = `${namespace}:stripped_${item}`
+		return {
+			type: "farmersdelight:cutting",
+			ingredients: [{ item: logID }],
+			result: [
+				{ item: strippedLogID },
+				{ item: "farmersdelight:tree_bark" }
 			],
-			"tool": {
-				"type": "farmersdelight:tool_action",
-				"action": "pickaxe_dig"
+			tool: {
+				type: "farmersdelight:tool_action",
+				action: "pickaxe_dig"
 			}
-		})
+		}
 	})
+	Recipes.forEach(recipe => event.custom(recipe))
 })
