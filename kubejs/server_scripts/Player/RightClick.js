9ac00c7a8a4d3f64a28e05fe594a762407ec0426
xiaoliziawa <xiaoliziawa@gmail.com>
2024-05-27 22:14:43 +0800

    WCS你的🐎

--- kubejs/server_scripts/Player/RightClick.js
+++ kubejs/server_scripts/Player/RightClick.js
@@ -1,21 +1,29 @@
 BlockEvents.rightClicked(e => {
 	const { player, block, item } = e
-
+	
 	// 原木堆生火
-	if (player.offHandItem == 'minecraft:stick' &&
-		player.mainHandItem == 'minecraft:flint' &&
-		block.id == 'new_create:log_pile') {
-		block.set('minecraft:fire')
-		item.count--
-		player.offhandItem.item--
-	}
+	rightClickCrafting('new_create:log_pile', 'minecraft:fire')
 
 	// 篝火
-	if (player.offHandItem == 'minecraft:stick' &&
-		player.mainHandItem == 'minecraft:flint' &&
-		block.id == 'minecraft:campfire') {
-		block.set('minecraft:campfire', { lit: true })
-		item.count--
-		player.offhandItem.item--
+	rightClickCrafting('minecraft:campfire', 'minecraft:campfire', { lit: true })
+
+	// Fire !!!!!!!!!!!!!😡
+	function rightClickCrafting(BlockId, BlockSetId) {
+		if (player.offHandItem == 'minecraft:stick' &&
+			player.mainHandItem == 'minecraft:flint' &&
+			block.id == BlockId) {
+			block.set(BlockSetId)
+			item.count--
+			player.offhandItem.item--
+		}
+
+		if (player.offHandItem == 'minecraft:stick' &&
+			player.mainHandItem == 'minecraft:flint' &&
+			block.id == BlockId) {
+			block.set(BlockSetId)
+			item.count--
+			player.offhandItem.item--
+		}
+
 	}
 })
